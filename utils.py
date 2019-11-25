b6d8a1d432737163abe1221162ee73fb85002ddd
You Xie <xyshell@bu.edu>
2019-11-24 19:25:50 -0500

    .

--- utils.py
+++ utils.py
@@ -6,3 +6,7 @@ DATA_PATH = os.path.join(ROOT, 'data')
 
 # IGNIS airdrop data
 IGNIS_4H = os.path.join(DATA_PATH, 'IGNIS_airdrop_4h.csv')
+
+# USDT
+USDT_1H = os.path.join(DATA_PATH, 'USDT_1h.csv')
+
